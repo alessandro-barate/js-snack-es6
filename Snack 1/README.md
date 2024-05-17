3795b9b01219d742dba2b9c89a833460a9826e82
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-17 11:07:40 +0200

    [UPD] Snack 1

--- Snack 1/README.md
+++ Snack 1/README.md
@@ -2,3 +2,6 @@ SNACK 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando template literal.
+
+Posso prendere la prima bici dell'array come riferimento per una variabile, che poi con un ciclo verrà confrontato con ogni elemento.
+Con la condizione peso bici di riferimento < peso elemento dell'array vado ad impostare il nuovo valore della bici di riferimento se la condizione è vera.
--- Snack 1/javascript/main.js
+++ Snack 1/javascript/main.js
@@ -3,45 +3,96 @@
 const bikes = [
   {
     nome: "Bianchi Oltre RC",
-    peso: "1.5 kg",
+    peso: 7.5,
+    unitaMisuraPeso: "kg",
   },
   {
     nome: "Look 795 Blade RS Proteam",
-    peso: "1.45 kg",
+    peso: 8.45,
+    unitaMisuraPeso: "kg",
   },
   {
     nome: "Wilier 0 SLR",
-    peso: "1.52 kg",
+    peso: 6.49,
+    unitaMisuraPeso: "kg",
   },
   {
     nome: "Pinarello Dogma F",
-    peso: "1.48 kg",
+    peso: 7.48,
+    unitaMisuraPeso: "kg",
   },
   {
     nome: "Colnago V4Rs",
-    peso: "1.51 kg",
+    peso: 8.51,
+    unitaMisuraPeso: "kg",
   },
   {
     nome: "Cervelo S5",
-    peso: "1.49 kg",
+    peso: 6.49,
+    unitaMisuraPeso: "kg",
   },
   {
     nome: "Canyon Ultimate CF SL R101",
-    peso: "1.46 kg",
+    peso: 7.46,
+    unitaMisuraPeso: "kg",
   },
 ];
 
+/*console.log(bikes[0].peso);
+console.log(bikes[1].peso);
+console.log(bikes[2].peso);
+console.log(bikes[3].peso);
+console.log(bikes[4].peso);
+console.log(bikes[5].peso);
+console.log(bikes[6].peso);*/
+
 const result = document.getElementById("result").innerText;
+let biciLeggera = bikes[0];
+// for
+/*for (let i = 1; i < bikes.length; i++) {
+  const biciAttuale = bikes[i];
+  if (biciAttuale.peso < biciLeggera.peso) {
+    biciLeggera = biciAttuale;
+  }
+   Qui non si sa quante bici, nel caso, hanno lo stesso peso */
 
-/*for (let key in bikes) {
-  console.log(bikes[key]);
-}*/
+/*if (biciAttuale.peso === biciLeggera.peso) {
+    console.log(biciAttuale);
+  } Non funziona nel caso ci sia un peso minore dopo il peso più leggero
+} */
 
-//console.log(bikes[1].nome);
-let bikesName;
+console.log(biciLeggera.nome);
 
-for (let i = 0; i < bikes.length; i++) {
-  bikesName = bikes[i].nome;
+console.log("--------------------");
+
+// for..of
+for (let index of bikes) {
+  console.log(index);
 }
 
-console.log(bikesName);
+console.log("--------------------");
+
+// forEach
+
+bikes.forEach((element, index, array) => {
+  console.log(element);
+});
+
+console.log("--------------------");
+
+let pesoLeggero = bikes[0].peso;
+for (let i = 1; i < bikes.length; i++) {
+  const pesoAttuale = bikes[i].peso;
+  if (pesoAttuale < pesoLeggero) {
+    pesoLeggero = pesoAttuale;
+  }
+}
+console.log(pesoLeggero);
+
+const biciLeggere = bikes.filter((bike, i, arr) => {
+  if (bike.peso === pesoLeggero) {
+    return true;
+  }
+});
+
+console.log(biciLeggere);
